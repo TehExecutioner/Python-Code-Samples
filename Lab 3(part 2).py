7e69e7263894df81c2ff43f3c3b2ea329637640b
Noah Martinez <91973097+TehExecutioner@users.noreply.github.com>
2021-10-15 14:21:13 -0500

    Add files via upload
    Lab 3 assignment

---
+++ Lab 3(part 2).py
@@ -0,0 +1,14 @@
+#this program calculates the area of a circle
+Radius = float(input("enter the radius "))
+PI = 3.14159
+area = PI*Radius*Radius
+print("radius is", area, "hope you have a nice day")
+#this program computes miles per gallon of a car
+Miles = float(input("enter the miles driven. "))
+Gallons = float(input("enter the gallons used of gas. "))
+MPG = Miles//Gallons
+print("Your MPG is", MPG, "Enjoy your day")
+#this formula converts Fahrenheit to Celsius
+F = float(input("enter temperature in Fahrenheit. "))
+C = (F-32)*5//9
+print("Your temperature in Celsius is", C, "Hope your're doing okay")
---
+++ Lab 3.py
@@ -0,0 +1,17 @@
+#Noah Martinez
+#10/21
+#this program prints "hello world"
+print('hello world')
+#this program asks for your name, then greets you with your name
+ask = input("what is your name? ")
+name = "noah"
+print('hello', name)
+#this program does the same thing as the previous, but has the instructor's name
+#as well
+ask = input("what is your name? ")
+name1 = "noah"
+name2 = "annie"
+if ask == name1:
+    print("hello", name1)
+elif ask == name2:
+    print("hello", name2)
